83f380fce5a3fab9f545bacd7940225afc4dcd6c
paulinawins <nguyennpaulina@gmail.com>
2019-08-08 15:40:46 -0400

    linkedIn button

--- actions.js
+++ actions.js
@@ -1,4 +1,4 @@
 function link(){
     alert("This button is working!");
-    location.href = "https://www.linkedin.com/in/nguyennpaulina";
+    //location.href = "https://www.linkedin.com/in/nguyennpaulina";
 }
