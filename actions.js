848880f910c2074e35ab3ac6e3feaecb4c23e3fd
paulinawins <nguyennpaulina@gmail.com>
2019-08-08 16:21:33 -0400

    css changes

--- actions.js
+++ actions.js
@@ -5,3 +5,14 @@ function gotolinkedin(){
 function projectPage(){
     alert("This page is still being developed :)");
 }
+
+function typeWelcome(){
+    var i=0;
+    var txt = "Welcome! Like everything, this is a project to learn something new!";
+    var speed = 50;
+    if(i< txt.length){
+        document.getElementsByClassName('welcome').innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typeWelcome, speed);
+    }
+}
