a6161e1470de7c86920e11d3e7609946c5333657
paulinawins <nguyennpaulina@gmail.com>
2019-08-09 08:37:02 -0400

    typing component

--- actions.js
+++ actions.js
@@ -1,20 +1,4 @@
-import Typewriter from 'typewriter-effect/dist/core';
-
 const welcome = document.getElementsByClassName('welcome');
-
-var typewriter = new Typewriter('welcome', {
-  loop: true
-});
-
-typewriter.typeString('Hello World!')
-    .pauseFor(2500)
-    .deleteAll()
-    .typeString('Strings can be removed')
-    .pauseFor(2500)
-    .deleteChars(7)
-    .typeString('<strong>altered!</strong>')
-    .pauseFor(2500)
-    .start();
 
 function gotolinkedin(){
     window.open ("https://www.linkedin.com/in/nguyennpaulina");
