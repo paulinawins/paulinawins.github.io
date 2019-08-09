a45a7913fb4ae0c69b7e0ca9d47195f05f9a515d
paulinawins <nguyennpaulina@gmail.com>
2019-08-09 08:41:15 -0400

    rm typing componenet

--- actions.js
+++ actions.js
@@ -7,15 +7,4 @@ function gotolinkedin(){
 function projectPage(){
     alert("This page is still being developed :)");
 }
-/*
-function typeWelcome(){
-    var i=0;
-    var txt = "Welcome! Like everything, this is a project to learn something new!";
-    var speed = 50;
-    if(i< txt.length){
-        document.getElementsByClassName('welcome').innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typeWelcome, speed);
-    }
-}
-*/
+
