abb77b17fa69aa2125907fb09ededaf457d9eee7
paulinawins <nguyennpaulina@gmail.com>
2019-08-17 17:42:40 -0400

    plz

--- actions.js
+++ actions.js
@@ -1,20 +1,7 @@
-//import TypeIt from 'typeit';
-
-
 function gotolinkedin(){
     window.open ("https://www.linkedin.com/in/nguyennpaulina");
 }
 
 function projectPage(){
     alert("This page is still being developed :)");
-}
-
-window.onload = function(){
-new TypeIt('#myElement', {
-  strings: 'Alex MacArthur'
-});
-
-new TypeIt('#input', {
-  strings: 'Paulina Nguyen'
-});
 }
