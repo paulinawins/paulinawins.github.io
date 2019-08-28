033eebbd4a2aa462d6a84da5bbd3655a5abd8b79
unknown <nguyennpaulina@gmail.com>
2019-08-27 21:46:35 -0700

    try new library

--- actions.js
+++ actions.js
@@ -5,13 +5,3 @@ function gotolinkedin(){
 function projectPage(){
     alert("This page is still being developed :)");
 }
-
-
-import Typed from 'typed.js';
-
-var options = {
-  strings: ["<i>First</i> sentence.", "&amp; a second sentence."],
-  typeSpeed: 40
-}
-
-var typed = new Typed(".element", options);
