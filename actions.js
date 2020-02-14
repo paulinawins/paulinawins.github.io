379d3799080d7586b03d329308253a8a27bebd4e
paulinawins <nguyennpaulina@gmail.com>
2020-02-14 13:10:16 -0800

    DOM test to practice JS

--- actions.js
+++ actions.js
@@ -6,3 +6,11 @@ function projectPage(){
     //alert("This page is still being developed :)");
     window.open("http://paulinawins.com/projects/");
 }
+
+const details = document.getElementById('welcome');
+const greeting  = document.createElement('p');
+greeting.setAttribute('class', 'lead');
+greeting.textContent = `Feel free to explore!`;
+
+details.appendChild(greeting);
+
