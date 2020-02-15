68772330e7e0cd808d3c86efc0e23f0deebb54b3
paulinawins <nguyennpaulina@gmail.com>
2020-02-15 14:47:48 -0800

    added dad jokes because why not?

--- actions.js
+++ actions.js
@@ -3,14 +3,45 @@ function gotolinkedin(){
 }
 
 function projectPage(){
-    //alert("This page is still being developed :)");
     window.open("http://paulinawins.com/projects/");
 }
 
-const details = document.getElementById('welcome');
+let clicked = 0;
+
+function punny(){
+
+    const jokeapi ='https://icanhazdadjoke.com/';
+    const joke  = document.createElement('p');
+    joke.setAttribute('class', 'jokes');
+
+    fetch(jokeapi, {
+        method:'get',
+        headers: {
+            Accept:'application/json'
+        },
+        responseType:'json'
+    }
+    )
+    .then(res => res.json())
+    .then(data => {
+        const details = document.getElementById('buttons');
+        //console.log(data.joke);
+        if(clicked>0){
+            details.removeChild(details.lastChild);
+        }
+        joke.textContent = data.joke;
+        details.appendChild(joke);
+        clicked++; 
+    })
+    .catch(
+        console.log("The joke api isn't working properly. Dads are sad")
+    )
+
+}
+
+/*
 const greeting  = document.createElement('p');
 greeting.setAttribute('class', 'lead');
 greeting.textContent = `Feel free to explore!`;
-
 details.appendChild(greeting);
-
+*/
